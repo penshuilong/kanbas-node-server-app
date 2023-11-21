cf2d5640bcb51b99895d966db3182402cbc5174a
penshuilong <zhao.zefe@northeastern.edu>
2023-11-20 21:16:52 -0500

    third commit

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 const app = express();
@@ -14,7 +13,6 @@ app.use(cors({
 }));
 CourseRoutes(app);
 ModuleRoutes(app);
-AssignmentRoutes(app);
 Lab5(app);
 HelloRoutes(app);
 app.listen(process.env.PORT || 4000);
